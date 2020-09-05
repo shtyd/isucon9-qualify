1cf81a4bb27cb02b9269bd217b3cb8bd4b0a7318
shtyd <shtyd001@gmail.com>
2020-09-06 00:18:43 +0900

    index間違い修正

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -49,13 +49,12 @@ CREATE TABLE `transaction_evidences` (
   `item_category_id` int unsigned NOT NULL,
   `item_root_category_id` int unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   INDEX idx_item_category_id (`item_category_id`),
   INDEX idx_item_id (`item_id`),
   INDEX idx_seller_id (`seller_id`),
   INDEX idx_buyer_id (`buyer_id`),
   INDEX idx_created_at (`created_at`)
-
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
 DROP TABLE IF EXISTS `shippings`;
